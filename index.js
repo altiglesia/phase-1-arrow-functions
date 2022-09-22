4e92255ad79267e518ae3229dbc6e93f754d36fb
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-09-22 11:03:17 -0400

    commit tree b763236ec4bbaf960340a48d9ca27304bc0dcf2e

---
+++ index.js
@@ -0,0 +1,37 @@
+// function foo() {
+//     return 'bar';
+//   }
+
+// const foo = function() {
+//     return 'bar';
+// }
+
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+// add(2,3); //=> 5
+
+// (parameter1, parameter2) => parameter1 + parameter2;
+
+// const twoAdder = x => x + 2;
+// // is the same as
+// const twoAdder = (x) => x + 2;
+
+// const nums = [1,2,3];
+// const squares = nums.map(x => x ** 2); 
+// squares; //=> [1,4,9]
+// nums; //=> [1,2,3]
+
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000, 100);
+
+const square = x => Math.pow(x, 2);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+// const nums = [1,2,3];
+// const squares = nums.map(x => x ** 2);
+
+// finishedItems = overdueTodoItems.map( item => item.className = "complete");
+// header.innerText = `You finished ${finishedItems.length} items!`;
+
+// lapsedUserAccounts.map( u => sendBillTo(u.address) );
+
